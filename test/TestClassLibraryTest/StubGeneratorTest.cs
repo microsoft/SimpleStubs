454922873fff7297524f27d3b48ff8e659c53ea5
mclift <mike@two-cats>
2017-02-02 16:47:30 +0000

    Test changing MockBehavior at runtime

--- test/TestClassLibraryTest/StubGeneratorTest.cs
+++ test/TestClassLibraryTest/StubGeneratorTest.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Etg.SimpleStubs;
 using Etg.SimpleStubs.CodeGen;
@@ -37,6 +38,41 @@ namespace TestClassLibraryTest
         {
             StubITestInterface stub = new StubITestInterface(MockBehavior.Strict);
             Assert.AreEqual(MockBehavior.Strict, stub.MockBehavior);
+        }
+
+        [TestMethod]
+        public void TestMockBehaviorProperty_CanBeChangedAtRuntime()
+        {
+            StubITestInterface stub = new StubITestInterface(MockBehavior.Strict);
+            ITestInterface testInterface = stub;
+            bool wasExceptionThrownInStrictMode = false;
+            bool wasExceptionThrownInStrictMode2ndTime = false;
+
+            // Check for exception in strict mode
+            try
+            {
+                testInterface.DoSomething();
+            }
+            catch (SimpleStubsException)
+            {
+                wasExceptionThrownInStrictMode = true;
+            }
+
+            // Switch to loose mode, do not expect exception
+            stub.MockBehavior = MockBehavior.Loose;
+
+            // SWitch back to strict mode and check for exception
+            try
+            {
+                testInterface.DoSomething();
+            }
+            catch (SimpleStubsException)
+            {
+                wasExceptionThrownInStrictMode2ndTime = true;
+            }
+
+            Assert.AreEqual(true, wasExceptionThrownInStrictMode);
+            Assert.AreEqual(true, wasExceptionThrownInStrictMode2ndTime);
         }
     }
 }
