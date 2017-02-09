419b808d0476a847a4a6233023b4c896c8f81dfa
Nehme Bilal <nbilal@microsoft.com>
2017-02-08 16:52:55 -0800

    Replace Etg.SimpleStubs.dll with Etg.SimpleStubs.cs
    This makes it easier to share the stubs between multiple projects.

--- src/SimpleStubs/Times.cs
+++
@@ -1,9 +0,0 @@
-namespace Etg.SimpleStubs
-{
-    public class Times
-    {
-        public const int Once = 1;
-        public const int Twice = 2;
-        public const int Forever = int.MaxValue;
-    }
-}
